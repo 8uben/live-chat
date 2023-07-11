e92a910d7ee05193b0f732b7a6ee5a65f75d9021
Vladislav Fedyaev <vladislav.fdv@mail.ru>
2023-07-11 16:57:58 +0300

    add gem devise

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable,
+  # :recoverable, :rememberable, :validatable
+
+  devise :database_authenticatable, :registerable, :validatable
+
+  validates :username, uniqueness: true
+end
